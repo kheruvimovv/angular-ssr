891d44ef1fa1ad31508907e652dfbce762ddcf02
Dmitriy <subsidia.d@gmail.com>
2026-01-26 13:44:12 +0300

    ng add @angular/ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,5 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideClientHydration(withEventReplay()),
   ],
 };
